c9f8187ea6a1fbb56357dcbb3ff14c62620493cb
karitogb <40370649+karitogb@users.noreply.github.com>
2018-06-19 16:47:12 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,26 +4,63 @@
 # analítica como funcion del numero de puntos (desde N=10 hasta N=10^8). 
 # Tanto el error como el numero de puntos deben variar en escala logaritmica.
 
-def integral(
+from numpy import *
+#import plotly
+#from matplotlib import pyplot
+#from matplotlib import savefig
+
+def integralexp(n):
+    equis=[]
+    efe=[]
+    # x in range (0,1) according to the assigment
+    tami=float(1.0/n)
+    print n
+    
+    for ke in range(0,n):
+        equis.append(float(tami*ke))
+        efe.append(float(exp(tami*ke)))
+    #efe=exp(equis)
+    return trapz(efe,equis)    
 
 tam=8
-for ke in range(1,tam+1):
-  if ke == 1:
-      vali=10
-  if ke == 2:
-      vali=10e2
-  if ke == 3:
-      vali=10e3
-  if ke == 4:
-      vali=10e4
-  if ke == 5:
-      vali=10e5
-  if ke == 6:
-      vali=10e6
-  if ke == 7:
-      vali=10e7
-  if ke == 8:
-      vali=10e8
-  
+plx=[]
+ply=[]
+vali=0
+
+fp=open("integrales.txt","w")
+for ke in range(1,tam-1):
+    if ke == 1:
+        vali=10
+    if ke == 2:
+        vali=int(10e2)
+    if ke == 3:
+        vali=int(10e3)
+    if ke == 4:
+        vali=int(10e4)
+    if ke == 5:
+        vali=int(10e5)
+    if ke == 6:
+        vali=int(10e6)
+    if ke == 7:
+        vali=int(10e7)
+    if ke == 8:
+        vali=int(10e8)
+
+    ply.append(integralexp(vali))
+    plx.append(vali)
+
+    fp.write("%f %f\n" % (vali,ply[ke-1]))
+fp.close()    
+#savefig(pyplot(plx,ply))
+
+#solucion analitica
+val=exp(1)-1
+
+#archivo para grafica de fraccion vrs. el numero de puntos
+fp=open("plot.dat","w")
+for ke in range(0,len(plx)):
+    fp.write("%f %f\n" % (plx[ke],ply[ke]/val))
+fp.close()
+
 
 
